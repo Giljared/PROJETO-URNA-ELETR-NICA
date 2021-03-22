ef2b4a909d1ec3c3cd3de329b0a14c327c3c172c
GILVANETE SIMPLICIO DA SILVA <64940515+Giljared@users.noreply.github.com>
2021-03-22 02:17:49 -0300

    subindo projeto urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'foto1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'foto4.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'foto4.jpg', legenda: 'Prefeito' },
+                    { url: 'foto4.jpg', legenda: 'Vice-prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QUERTY',
+                vice: 'Zul',
+                fotos: [
+
+                    { url: 'foto4.jpg', legenda: 'Prefeito' },
+                    { url: 'foto4.jpg', legenda: 'Vice-prefeito', small: true }
+
+                ]
+            },
+        ]
+    }
+];
